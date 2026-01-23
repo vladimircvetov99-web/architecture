cc55363f1d9290b4ea4f8811e2aede9b9e051808
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-01-23 15:35:28 +0600

    fixed css js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ const menu = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 } );
+
+
+const swiper = new Swiper('.projects__slider', {
+ 
+  loop: true,
+
+   // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
